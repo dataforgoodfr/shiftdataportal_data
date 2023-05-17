f064be032540783b9527ce42a7ac7f43ae0d7aa8
gaudinnicolas <nicolasgaudin@yahoo.fr>
2023-05-17 13:16:19 +0200

    SDP Raw: cleaned zip.py

--- src/sdp_data/zip.py
+++ src/sdp_data/zip.py
@@ -1,11 +1,9 @@
-import os
 import py7zr
 
 def create_7z_archive(source_dir, output_file):
      with py7zr.SevenZipFile(output_file, 'w') as archive:
         archive.writeall(source_dir)
 
-# Usage example
 source_directory = "data/_raw"
 output_archive = "data/_raw.7z"
 create_7z_archive(source_directory, output_archive)
