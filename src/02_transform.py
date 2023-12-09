f1c937202de69a82d8bcf165d6cbcb792f97a390
sebastienbourgeois <sebastienbourgeois60@gmail.com>
2023-12-09 16:15:14 +0100

    Update csv file of country group data

--- src/02_transform.py
+++ src/02_transform.py
@@ -11,17 +11,12 @@ df = pd.read_csv(
     "../data/dataiku/raw/country_project/country_groups_new.csv",
     sep=",",
 )
-print(f"Size table before: {len(df)}")
 country_groups = update_country_groups(df)
-print(f"Size table before: {len(country_groups)}")
-# check_df = country_groups[country_groups.country.isin(["Ecuador", "Qatar"])]
-check_df = country_groups[country_groups.group_name.isin(["EU27"])]
-print(check_df)
-# country_groups.to_csv(
-#     "../data/final/country_groups_prod.csv",
-#     sep=",",
-#     index=False,
-# )
+country_groups.to_csv(
+    "../data/final/country_groups_prod.csv",
+    sep=",",
+    index=False,
+)
 
 ###########################
 # Multi-selection groups
