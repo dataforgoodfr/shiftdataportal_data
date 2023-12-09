08383d4b08b7e60dd9a8d756cfe809671ac33937
sebastienbourgeois <sebastienbourgeois60@gmail.com>
2023-12-09 13:09:41 +0100

    Update country group dataset

--- src/02_transform.py
+++ src/02_transform.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+from src.transformations.country_groups import update_country_groups
 from src.transformations.multi_selection_country_groups import process_multi_selection_country_groups
 
 ###########################
@@ -10,23 +11,29 @@ df = pd.read_csv(
     "../data/dataiku/raw/country_project/country_groups_new.csv",
     sep=",",
 )
-df.to_csv(
-    "../data/final/country_groups_prod.csv",
-    sep=",",
-    index=False,
-)
+print(f"Size table before: {len(df)}")
+country_groups = update_country_groups(df)
+print(f"Size table before: {len(country_groups)}")
+# check_df = country_groups[country_groups.country.isin(["Ecuador", "Qatar"])]
+check_df = country_groups[country_groups.group_name.isin(["EU27"])]
+print(check_df)
+# country_groups.to_csv(
+#     "../data/final/country_groups_prod.csv",
+#     sep=",",
+#     index=False,
+# )
 
 ###########################
 # Multi-selection groups
 ###########################
 
-df = pd.read_csv(
-    "../data/dataiku/raw/country_project/multiselect_groups.csv",
-    sep=",",
-)
-multi_selection_country_groups = process_multi_selection_country_groups(df)
-multi_selection_country_groups.to_csv(
-    "../data/final/multiselect_groups_prod.csv",
-    sep=",",
-    index=False,
-)
+# df = pd.read_csv(
+#     "../data/dataiku/raw/country_project/multiselect_groups.csv",
+#     sep=",",
+# )
+# multi_selection_country_groups = process_multi_selection_country_groups(df)
+# multi_selection_country_groups.to_csv(
+#     "../data/final/multiselect_groups_prod.csv",
+#     sep=",",
+#     index=False,
+# )
---
+++ src/transformations/country_groups.py
@@ -0,0 +1,76 @@
+from typing import Union, List
+
+import pandas as pd
+
+
+def _add_new_members_to_group(
+        country_groups: pd.DataFrame,
+        new_members: Union[str, List[str]],
+        group_name: str,
+) -> pd.DataFrame:
+    """
+    Returns True if the group or organisation of countries contains only country, False otherwise.
+    """
+    if isinstance(new_members, str):
+        new_members = [new_members]
+
+    rows = []
+    for new_member in new_members:
+        row = {
+            "group_type": "group",
+            "group_name": group_name,
+            "country": new_member,
+        }
+        rows.append(row)
+    new_members_to_add = pd.DataFrame(data=rows)
+    return pd.concat([country_groups, new_members_to_add], ignore_index=True)
+
+
+def update_country_groups(raw_country_groups: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns the final processed dataset containing the multi-selection country groups.
+    :param raw_country_groups: DataFrame that contains the raw data of the multi-selection country groups.
+    :return: DataFrame with the final data of the multi-selection country groups.
+    """
+    country_groups = raw_country_groups.copy()
+
+    # Removal of UK from EU
+    country_groups = country_groups[
+        ~(
+                (country_groups.group_name == "EU28")
+                & (country_groups.country == "United Kingdom")
+        )
+    ]
+
+    # Replace EU28 with EU27
+    country_groups.loc[country_groups.group_name == "EU28", "group_name"] = "EU27"
+
+    # Add Colombia, Costa Rica, Estonia, Israel, Latvia, Lithuania, Slovenia to OECD members
+    new_oecd_members = [
+        "Colombia",
+        "Costa Rica",
+        "Estonia",
+        "Israel",
+        "Latvia",
+        "Lithuania",
+        "Slovenia",
+    ]
+    country_groups = _add_new_members_to_group(country_groups, new_members=new_oecd_members, group_name="OECD")
+
+    # Add Congo, Equatorial Guinea, Gabon, Libya to OPEC members
+    new_opec_members = [
+        "Congo",
+        "Equatorial Guinea",
+        "Gabon",
+        "Libya",
+    ]
+    country_groups = _add_new_members_to_group(country_groups, new_members=new_opec_members, group_name="OPEC")
+
+    # Removal of Ecuador and Qatar from OPEC
+    country_groups = country_groups[
+        ~(
+                (country_groups.group_name == "OPEC")
+                & (country_groups.country.isin(["Ecuador", "Qatar"]))
+        )
+    ]
+    return country_groups
